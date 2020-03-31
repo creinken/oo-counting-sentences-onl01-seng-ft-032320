ae6becedda58bb75b62809db039488db6b81818a
Christopher Reinken <christopherreinken@gmail.com>
2020-03-31 16:50:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
     counter = 0
-    self.split('!', ".", '?').each {|sentence| counter +=1 }
+    self.split(/[!?.] /).each {|sentence| counter +=1 }
   end
 end
